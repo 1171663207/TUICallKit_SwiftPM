dea07aaabb84c17bbb0d3b50cf33e70eb693140e
noahcheng <noahcheng@tencent.com>
2025-06-10 20:34:40 +0800

    Update package.swift Release 3.1.0

---
+++ Sources/TUICallKit_SwiftPM/State/GlobalState.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalState.swift
+//  Pods
+//
+//  Created by vincepzhang on 2025/2/6.
+//
+
+import Foundation
+
+class GlobalState: NSObject {
+    var enableMuteMode: Bool = UserDefaults.standard.value(forKey: ENABLE_MUTEMODE_USERDEFAULT) as? Bool == true ? true : false
+    var enableFloatWindow: Bool = false
+    var enableIncomingBanner: Bool = false
+    var enableVirtualBackground: Bool = false
+    var enableForceUseV2API: Bool = false
+    var enableMultiDeviceAbility: Bool = false
+}
